97d277070925502a62e9367436227e189581ca8f
JimHui12 <jianghua.hui@uq.net.au>
2024-02-22 20:41:16 +1000

    c-sharp-basic

---
+++ c-sharp-basic/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+String firstFriend = "Maria";
+String secondFriend = "Sage";
+
+Console.WriteLine($"{firstFriend} and {secondFriend}");
