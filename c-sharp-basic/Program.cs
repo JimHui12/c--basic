45871c07bdf38e57faaacb9b764826ad3ede649f
JimHui12 <jianghua.hui@uq.net.au>
2024-02-26 21:35:17 +1000

    integer

--- c-sharp-basic/Program.cs
+++ c-sharp-basic/Program.cs
@@ -1,13 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+String
+*/
 Console.WriteLine("Hello, World!");
-
 string firstFriend = "Maria";
 string secondFriend = "Sage";
-
 Console.WriteLine($"{firstFriend} and {secondFriend}");
-
 string greeting = "    Maria   ";
 greeting = greeting.TrimStart();
 greeting = greeting.Trim();
-string greetingTwo = "   scoat      ";
-Console.WriteLine($"{greeting} and {greetingTwo.Trim()}");
+string greetingTwo = "   Scoat   Scott   ";
+string friends = $"{greeting} and {greetingTwo.Trim()}";
+Console.WriteLine(friends);
+friends = friends.Replace("Scott", "Damian");
+Console.WriteLine(friends);
+Console.WriteLine(friends.ToUpper());
+Console.WriteLine(friends.StartsWith("Sco"));
+
+/*
+Integer
+*/
+int a = 2100000000;
+int b = 2100000000;
+int c = a + b;
+Console.WriteLine(c);
