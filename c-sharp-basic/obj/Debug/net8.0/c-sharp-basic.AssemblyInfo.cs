72ccd7f737061282e43e856874fde086b2f91522
JimHui12 <jianghua.hui@uq.net.au>
2024-03-13 21:42:32 +1000

    category form added

--- c-sharp-basic/obj/Debug/net8.0/c-sharp-basic.AssemblyInfo.cs
+++ c-sharp-basic/obj/Debug/net8.0/c-sharp-basic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c-sharp-basic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0f3a2367cc7e1cf377987213a17d9996d5afc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d593c93834ea7b79b2e20c85df417b008861832c")]
 [assembly: System.Reflection.AssemblyProductAttribute("c-sharp-basic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c-sharp-basic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- web-app/web-app/obj/Debug/net8.0/web-app.AssemblyInfo.cs
+++ web-app/web-app/obj/Debug/net8.0/web-app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0f3a2367cc7e1cf377987213a17d9996d5afc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d593c93834ea7b79b2e20c85df417b008861832c")]
 [assembly: System.Reflection.AssemblyProductAttribute("web-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
